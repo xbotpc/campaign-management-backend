ddda5ed156e6241cbd8cac02927ea5c5f357501c
xbotpc <47697111+xbotpc@users.noreply.github.com>
2025-01-26 16:18:29 +0200

    FIRST COMMIT

---
+++ index.ts
@@ -0,0 +1,11 @@
+import cors from "cors";
+import express, { Application, Response } from "express";
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+
+const port = 3001;
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
+
+app.get('/', (_req, res: Response) => { res.status(200).json({ message: 'OK' }) })
