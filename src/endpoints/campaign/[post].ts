83de18901488af1b82ee2e144db59aeec1347b34
Piyush <piyushschoughule@gmail.com>
2025-01-29 18:06:13 +0200

    validation in middleware for payouts

--- src/endpoints/campaign/[post].ts
+++ src/endpoints/campaign/[post].ts
@@ -6,7 +6,7 @@ export function schemaValidation() {
         body("title")
             .exists({
                 values: "null",
-            }).withMessage("Title cannot be null")
+            }).withMessage("Title cannot be null or undefined")
             .isString().withMessage("Title should be of type string")
             .isLength({
                 max: 200,
@@ -21,6 +21,25 @@ export function schemaValidation() {
             .optional()
             .isBoolean().withMessage(
                 "Invalid is_active value",
+            ),
+        body("payouts").isArray({ min: 1 }).withMessage(
+            "payouts should be an array with length at least 1",
+        ),
+        body("payouts.*.country")
+            .isString().withMessage(
+                "country data type should be string",
+            ).isISO31661Alpha3().withMessage(
+                "please provide ISO 3166-1 A3 format for country",
+            ),
+        body("payouts.*.amount")
+            .isDecimal().withMessage(
+                "amount data type should be decimal",
+            ),
+        body("payouts.*.currency")
+            .isString().withMessage(
+                "currency data type should be string",
+            ).isISO4217().withMessage(
+                "please provide ISO 4217 format for currency",
             ),
     ];
 }
