4c3e620f9f40c468d7d1a8d0ea48721eefa8b052
Piyush <piyushschoughule@gmail.com>
2025-01-30 13:12:50 +0200

    update

--- src/database/migrations/20250128163153_init.cjs
+++ src/database/migrations/20250128163153_init.cjs
@@ -1,33 +1,35 @@
 // Problems with knex.
 // Cannot run TS files as migrations
-async function up(knex){
-    return knex.schema.createTable('campaigns', (table) => {
-        table.uuid('id', {
-            primaryKey: true
-        });
-        table.string('title', 200).notNullable();
-        table.string('url').notNullable();
-        table.boolean('is_active').defaultTo('false');
-        table.uuid('payout_id').notNullable();
-    }).createTable('payouts', (table) => {
-        table.uuid('id', {
-            primaryKey: true
-        })
-        table.string('country', 60).notNullable();
-        table.decimal('amount', 50, 4).unsigned().notNullable();
-        table.string('currency', 3).notNullable();
-    }).table('campaigns', (table) => {
-        table.foreign('payout_id').references('payouts.id');
+async function up(knex) {
+    await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    return knex.schema.createTable("campaigns", (table) => {
+        table.uuid("id", {
+            primaryKey: true,
+        }).unique().defaultTo(knex.raw("uuid_generate_v4()"));
+        table.timestamps(true, true);
+        table.string("title", 200).notNullable();
+        table.string("url").notNullable();
+        table.boolean("is_active").defaultTo("false");
+    }).createTable("payouts", (table) => {
+        table.uuid("id", {
+            primaryKey: true,
+        }).unique().defaultTo(knex.raw("uuid_generate_v4()"));
+        table.timestamps(true, true);
+        table.uuid("campaign_id").notNullable().references("id").inTable('campaigns');
+        table.string("country", 60).notNullable();
+        table.decimal("amount", 50, 4).unsigned().notNullable();
+        table.string("currency", 3).notNullable();
     });
 }
 
 async function down(knex) {
+    await knex.schema.table('payouts', (table) => table.dropForeign(["campaignID"]));
     return knex.schema
-        .dropTable('campaigns')
-        .dropTable('payouts');
+        .dropTable("campaigns")
+        .dropTable("payouts");
 }
 
 module.exports = {
     up,
-    down
-}
+    down,
+};
